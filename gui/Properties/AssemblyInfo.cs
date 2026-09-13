0efca0056b0f6a3d6dc0cca062f3ea7fa3e6ac92
espero_dev <67075721+espero-dev@users.noreply.github.com>
2026-09-12 22:59:24 -0700

    Add files via upload

---
+++ gui/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows.Media;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Chrysler Scanner GUI")]
+[assembly: AssemblyDescription("Software for interfacing Chrysler's legacy CCD/PCI/SCI-bus.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BoundaryCondition")]
+[assembly: AssemblyProduct("Chrysler Scanner GUI")]
+[assembly: AssemblyCopyright("Copyright © 2020-2023 Boundary Condition")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4405bc29-5e44-4ec8-9f6a-bcd1712a95a2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.14.4")]
+//[assembly: AssemblyFileVersion("1.0.0")]
